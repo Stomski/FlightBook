9b8f69d769a656caef01b8918df0ef1fd64ab904
Robert Stomski <stomski.robert@gmail.com>
2024-06-01 20:29:12 -0600

    migrations modified

--- FlightBook/migrations/versions/20240601_152631_creating_first_tables.py
+++ FlightBook/migrations/versions/20240601_152631_creating_first_tables.py
@@ -1,12 +1,16 @@
 """creating first tables
 
 Revision ID: debd26d9b565
-Revises: 
+Revises:
 Create Date: 2024-06-01 15:26:31.199034
 
 """
 from alembic import op
 import sqlalchemy as sa
+
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 
 # revision identifiers, used by Alembic.
@@ -78,6 +82,9 @@ def upgrade():
     sa.UniqueConstraint('email'),
     sa.UniqueConstraint('username')
     )
+    if environment == "production":
+        op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
+    # ### end Alembic commands ###qqqqqqqqq
     # ### end Alembic commands ###
 
 
