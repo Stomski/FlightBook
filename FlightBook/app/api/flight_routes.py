063112738167a826081180760b8c8628d6cd5f45
Robert Stomski <stomski.robert@gmail.com>
2024-06-02 11:43:39 -0600

    working route get flights by user

--- FlightBook/app/api/flight_routes.py
+++ FlightBook/app/api/flight_routes.py
@@ -2,4 +2,15 @@ from flask import Blueprint, jsonify
 from flask_login import login_required
 from app.models import Flight
 
-flight_routes = Blueprint('users', __name__)
+flight_routes = Blueprint('flights', __name__)
+
+
+@flight_routes.route('/by-user/<int:userId>')
+def test(userId):
+    """
+    this route will return all flights for a given user
+    """
+    flights = Flight.query.filter(Flight.user_id ==userId).all()
+    print(flights)
+
+    return {'message':"success"}
