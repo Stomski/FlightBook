f74d7a08dc2b81c1b8d4391dc5eb1fe65da287e9
Robert Stomski <stomski.robert@gmail.com>
2024-06-02 22:16:34 -0600

    main page component rendering

---
+++ FlightBook/react-vite/src/components/MainPage/MainPage.jsx
@@ -0,0 +1,9 @@
+function MainPage() {
+  return (
+    <>
+      <h1>THIS IS A TEST I UNDERSTAND REACT</h1>
+    </>
+  );
+}
+
+export default MainPage;
--- FlightBook/react-vite/src/router/index.jsx
+++ FlightBook/react-vite/src/router/index.jsx
@@ -1,7 +1,8 @@
-import { createBrowserRouter } from 'react-router-dom';
-import LoginFormPage from '../components/LoginFormPage';
-import SignupFormPage from '../components/SignupFormPage';
-import Layout from './Layout';
+import { createBrowserRouter } from "react-router-dom";
+import LoginFormPage from "../components/LoginFormPage";
+import SignupFormPage from "../components/SignupFormPage";
+import Layout from "./Layout";
+import MainPage from "../components/MainPage/MainPage";
 
 export const router = createBrowserRouter([
   {
@@ -9,15 +10,7 @@ export const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <h1>Welcome!</h1>,
-      },
-      {
-        path: "login",
-        element: <LoginFormPage />,
-      },
-      {
-        path: "signup",
-        element: <SignupFormPage />,
+        element: <MainPage />,
       },
     ],
   },
