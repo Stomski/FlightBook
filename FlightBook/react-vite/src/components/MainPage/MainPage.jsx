0367481b490e73c7fdc1acb81d93440588653686
Robert Stomski <stomski.robert@gmail.com>
2024-06-02 23:06:24 -0600

    made some components, began rendering them on a page. feeling pretty good

--- FlightBook/react-vite/src/components/MainPage/MainPage.jsx
+++ FlightBook/react-vite/src/components/MainPage/MainPage.jsx
@@ -1,8 +1,11 @@
+import "./MainPage.css";
+
 function MainPage() {
   return (
-    <>
+    <section className="mainpagesection">
+      <header></header>
       <h1>THIS IS A TEST I UNDERSTAND REACT</h1>
-    </>
+    </section>
   );
 }
 
