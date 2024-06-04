07d026a3d6d99e1a8643bdf7cf16130cb9c5921b
Robert Stomski <stomski.robert@gmail.com>
2024-06-04 17:06:20 -0600

    all sites feed displaying, scrolling, updating

--- FlightBook/react-vite/src/components/FeedComponents/FeedAllSites.jsx
+++ FlightBook/react-vite/src/components/FeedComponents/FeedAllSites.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getAllSitesThunk } from "../../redux/sites";
+import "./FeedAllSites.css";
 
 export default function FeedAllSites() {
   const sites = useSelector((state) => state.sites);
@@ -11,13 +12,14 @@ export default function FeedAllSites() {
   }, []);
 
   return (
-    <>
+    <section className="all-sites-feed">
       <h1>TESTING FEED ALL SITES</h1>
       {Object.values(sites).map((site) => (
         <div className="site-card-div" key={site["id"]}>
           <h1>{site.name}</h1>
+          <h2>official ushpa? {site.official}</h2>
         </div>
       ))}
-    </>
+    </section>
   );
 }
