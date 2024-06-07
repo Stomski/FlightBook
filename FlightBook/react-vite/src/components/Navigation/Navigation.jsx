c7d465c69171eafbad52b9d32e18a828c879bffb
Robert Stomski <stomski.robert@gmail.com>
2024-06-07 17:17:21 -0600

    final friday afternoon

--- FlightBook/react-vite/src/components/Navigation/Navigation.jsx
+++ FlightBook/react-vite/src/components/Navigation/Navigation.jsx
@@ -3,10 +3,23 @@ import ProfileButton from "./ProfileButton";
 import "./Navigation.css";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import { thunkLogin } from "../../redux/session";
+import { useDispatch } from "react-redux";
+
+const demoUser = () => {
+  console.log("DEMO USER CALLED");
+  dispatch(thunkLogin({ email: "bobby@aa.io", password: "password" }));
+};
 
 function Navigation() {
+  const dispatch = useDispatch();
   const sessionUser = useSelector((state) => state.session.user);
   console.log(sessionUser, "SESSION USER IN NAVIGATION COMPONENT");
+
+  const demoUser = () => {
+    console.log("DEMO USER CALLED");
+    dispatch(thunkLogin({ email: "bobby@aa.io", password: "password" }));
+  };
   return (
     <div className="navbardiv">
       <div className="logo-container">
@@ -24,6 +37,11 @@ function Navigation() {
               alt="user profile image"
             />
           </div>
+        )}
+        {!sessionUser && (
+          <h3 onClick={demoUser} className="demo-user, clickable">
+            demo user
+          </h3>
         )}
 
         <div className="user-pulldown-menu">
